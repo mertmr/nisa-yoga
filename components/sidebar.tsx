7051ce83f79e797b3ca1f4b266d6a4c8120f49dc
Mert Meral <mertmr@gmail.com>
2023-08-05 14:38:04 +0000

    better sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -39,7 +39,7 @@ function Sidebar() {
           onClick={toggleSidebar}
           className="ml-3 mt-2 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         >
-          <span className="sr-only">Open sidebar</span>
+         
           <svg
             className="h-6 w-6"
             aria-hidden="true"
@@ -53,6 +53,7 @@ function Sidebar() {
               d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
             ></path>
           </svg>
+          <span className="pl-1">Menu</span>
         </button>
         <div></div>
       </div>
